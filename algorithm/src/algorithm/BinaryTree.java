e0ae0f355e2b20b0da35880264fa9d9c96300520
liupeng <liupengfun@163.com>
2016-12-15 00:59:01 +0800

    make gitignore,and binary tree > <

---
+++ algorithm/src/algorithm/BinaryTree.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+/**
+ * Created by liupeng on 2016/12/15.
+ * 初级算法学习  二叉树
+ */
+public class BinaryTree {
+    public static void main(String arg[]){
+        System.out.println("ok");
+    }
+}
